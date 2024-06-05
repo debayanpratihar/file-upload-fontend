0279f845e83d33dcec120290db2150f668dd3ce3
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2024-06-05 14:20:39 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>File Upload to Firebase</h1>
+        <h2>DHC Virtual Project Challenge: Practice Your Code</h2>
+        <p>Challenge Description:</p>
+        <p>
+          VoIP Project: <br />
+          Objective: Incorporate a feature into a web app that allows users to:
+          <ul>
+            <li>Make video calls to their friends.</li>
+            <li>Share their screen, specifically the YouTube website.</li>
+            <li>Record the video session and save it locally.</li>
+          </ul>
+          Timing Constraint: Video calls can only be made between 6 PM and 12 AM. Outside of these hours, the video call option should be disabled.
+        </p>
         <FileUpload />
       </header>
     </div>
